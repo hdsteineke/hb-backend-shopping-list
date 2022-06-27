30869854e7b3a4c5b8fafb7ae66aaef9c71caf26
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-27 16:16:58 -0700

    test passing for allowing current user to add new item

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -41,10 +41,12 @@ describe('items', () => {
   afterAll(() => {
     pool.end();
   });
-  it.skip('POST /api/v1/items creates a new shopping item with the current user', async () => {
+  it('POST /api/v1/items creates a new shopping item with the current user', async () => {
     const [agent, user] = await registerAndLogin();
+    console.log('user', user);
     const newItem = { description: 'eggs', qty: 12 };
     const resp = await agent.post('/api/v1/items').send(newItem);
+
     expect(resp.status).toEqual(200);
     expect(resp.body).toEqual({
       id: expect.any(String),
